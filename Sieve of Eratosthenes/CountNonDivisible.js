fafdfd3bcb500a5684208cce7eda6bf08388312e
Bilal Saad <bilalsaad2005@gmail.com>
2021-06-01 20:35:37 +0300

    CountNonDivisible Solution

--- Sieve of Eratosthenes/CountNonDivisible.js
+++ Sieve of Eratosthenes/CountNonDivisible.js
@@ -3,21 +3,31 @@
 
 function solution(A) {
     // write your code in JavaScript (Node.js 8.9.4)
-    const divisors = A.map(e => 0);
+    const N = A.length;
 
-    for (let i = 0; i<A.length; i++) {
-        let e = A[i];
-        for (let j = i+1; j<A.length; j++) {
-            let f = A[j];
-            if (f % e === 0) {
-                divisors[j]++;
-            }
-            if (e % f === 0) {
-                divisors[i]++;
-            }
-        }
+    let numCnt = new Array((N * 2)+1).fill(0);
+
+    for(let i in A) {
+        numCnt[A[i]]++;
     }
 
-    const res = divisors.map(e => A.length - e - 1);
-    return res;
+    let divCnt= [];
+
+    for(let i = 0; i < N; i++) {
+        let divisors = 0;
+
+        for(let j = 1; j * j <= A[i]; j++) {
+            if(A[i] % j === 0) {
+                divisors += numCnt[j];
+
+                if(A[i] / j !== j) {
+                    divisors += numCnt[A[i]/j];
+                }
+            }
+        }
+
+        divCnt[i] = N - divisors;
+    }
+
+    return divCnt;
 }
