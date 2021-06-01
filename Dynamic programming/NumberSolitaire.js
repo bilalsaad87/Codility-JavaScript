e2781f4ba295aa989c2cc430496acc93fa8a1e9d
Bilal Saad <bilalsaad2005@gmail.com>
2021-06-01 23:14:34 +0300

    NumberSolitaire Solution

--- Dynamic programming/NumberSolitaire.js
+++ Dynamic programming/NumberSolitaire.js
@@ -10,15 +10,13 @@ function solution(A) {
 
     for (var i = 1; i < n; i++) {
         var max = dp[i - 1] + A[i];
-
         for (var j = 1; j <= 6; j++) {
             if (i - j >= 0) {
                 max = Math.max(dp[i - j] + A[i], max);
             }
         }
-
         dp[i] = max;
     }
-
+    
     return dp[n - 1];
 }
